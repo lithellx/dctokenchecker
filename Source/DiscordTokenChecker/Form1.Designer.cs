8a1ad37b41f3fd9d49d0a954a6b8c500ca7184d5
lithé <45209073+lithellx@users.noreply.github.com>
2020-08-27 11:35:20 +0300

    Add files via upload

---
+++ Source/DiscordTokenChecker/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace DiscordTokenChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(92, 213);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(345, 24);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 48);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lithe\'s Discord Token Checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Source/DiscordTokenChecker/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DiscordTokenChecker
+{
+    public partial class Form1 : Form
+    {
+        public static bool bool1 = false;
+        public static int int_0;
+        public static int int_1;
+        public static int int_2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Select();
+            if (!File.Exists("verified.txt"))
+            {
+                File.Create("verified.txt");
+            }
+            if (!File.Exists("unverified.txt"))
+            {
+                File.Create("unverified.txt");
+            }
+            if (!File.Exists("invalid.txt"))
+            {
+                File.Create("invalid.txt");
+            }
+            if (!File.Exists("tokens.txt"))
+            {
+                File.Create("tokens.txt");
+                MessageBox.Show("The app is restarting to create the tokens files.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Restart();
+                Environment.Exit(0);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = "tokens.txt";
+            if (!File.Exists("tokens.txt"))
+            {
+                MessageBox.Show("Please restart the app to create the tokens files.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                File.WriteAllText("verified.txt", "");
+                File.WriteAllText("unverified.txt", "");
+                File.WriteAllText("invalid.txt", "");
+                using (FileStream fileStream = File.OpenRead(path))
+                {
+                    using (StreamReader streamReader = new StreamReader(fileStream, Encoding.UTF8, true, 128))
+                    {
+                        string text;
+                        while ((text = streamReader.ReadLine()) != null)
+                        {
+                            using (WebClient webClient = new WebClient())
+                            {
+                                NameValueCollection nameValueCollection = new NameValueCollection();
+                                nameValueCollection[""] = "";
+                                webClient.Headers.Add("Authorization", text);
+                                try
+                                {
+                                    webClient.UploadValues("https://discordapp.com/api/v6/invite/discordtokenchecker", nameValueCollection);
+                                }
+                                catch (WebException ex)
+                                {
+                                    string text2 = new StreamReader(ex.Response.GetResponseStream()).ReadToEnd();
+                                    if (text2.Contains("401: Unauthorized"))
+                                    {
+                                        int_2++;
+                                        File.AppendAllText("invalid.txt", text + Environment.NewLine);
+                                    }
+                                    else if (text2.Contains("You need to verify your account in order to perform this action."))
+                                    {
+                                        int_1++;
+                                        File.AppendAllText("unverified.txt", text + Environment.NewLine);
+                                    }
+                                    else
+                                    {
+                                        int_0++;
+                                        File.AppendAllText("verified.txt", text + Environment.NewLine);
+                                    }
+                                }
+                            }
+                        }
+                        int num = int_0 + int_2 + int_1;
+                        MessageBox.Show(string.Concat(new object[]
+                        {
+                            "Verified Tokens: ",
+                            int_0,
+                            "\nUnverified Tokens: ",
+                            int_1,
+                            "\nInvalid Tokens: ",
+                            int_2,
+                            "\nTotal: ",
+                            num
+                        }), "Tokens Checker", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                        int_0 = 0;
+                        int_1 = 0;
+                        int_2 = 0;
+                    }
+                }
+            }
+        }
+
+    }
+}
